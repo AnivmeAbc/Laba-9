2dfa178d082a615172685edf592547529deaf706
Anivme <anivmeoff@outlook.com>
2025-11-18 23:40:15 +0400

    Add multiply and divide functions with usage examples

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, Team!")
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
+if __name__ == "__main__":
+    print("Hello, Team!")
+    # Вызов функции Студента А
+    result_mult = multiply(5, 3)
+    print(f"Multiply result: {result_mult}")
+    # Вызов функции Студента Б
+    result_div = divide(10, 2)
+    print(f"Divide result: {result_div}")
